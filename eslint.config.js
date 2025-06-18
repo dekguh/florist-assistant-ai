b9f90d203178c7c84074ed9ba741fa9463b6b0a5
dekguh <kdkteguhmahesa@gmail.com>
2025-06-18 14:18:42 +0800

    config storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,32 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx,js}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      indent: ['error', 2],
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      quotes: ['error', 'single'],
-      'jsx-quotes': ['error', 'prefer-single'],
-      semi: ['error', 'never'],
-      'no-unused-vars': 'error',
-    },
-  }
-)
+export default tseslint.config({ ignores: ['dist'] }, {
+  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  files: ['**/*.{ts,tsx,js}'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+  },
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    indent: ['error', 2],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    quotes: ['error', 'single'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    semi: ['error', 'never'],
+    'no-unused-vars': 'error',
+  },
+}, storybook.configs["flat/recommended"]);
