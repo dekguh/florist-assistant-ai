c7d3454a6dd010f78ed7365cf22a8236c4a2d70d
dekguh <kdkteguhmahesa@gmail.com>
2025-06-19 07:52:20 +0800

    icon button style

--- src/components/ui/icon-button/icon-button.tsx
+++ src/components/ui/icon-button/icon-button.tsx
@@ -4,7 +4,15 @@ import classNames from 'classnames'
 
 const IconButton : React.FC<IIconButton> = ({ icon : Icon, ...rest }) => {
   return (
-    <button {...rest} className={classNames(rest.className)}><Icon /></button>
+    <button
+      {...rest}
+      className={classNames(
+        'hover:cursor-pointer h-10 w-10 rounded-full',
+        'flex items-center justify-center',
+        'text-gray-600 hover:text-blue-400',
+        rest.className
+      )}
+    ><Icon /></button>
   )
 }
 
