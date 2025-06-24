9dedd91d6fb6b79d8189a2d963b5845d7d97ef9a
just kadek <kdkteguhmahesa@gmail.com>
2025-06-24 10:33:23 +0800

    update type textfield

--- src/components/ui/textfield/types.d.ts
+++ src/components/ui/textfield/types.d.ts
@@ -2,4 +2,6 @@ import type { HTMLAttributes, HTMLProps } from 'react'
 
 export interface ITextfield extends HTMLAttributes<HTMLInputElement> {
   rootClassName?: HTMLProps<HTMLDivElement>['className'];
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  value: string;
 }
