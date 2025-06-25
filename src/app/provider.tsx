df2c6e81a8345cee58074ec74f50127480ab41cf
just kadek <kdkteguhmahesa@gmail.com>
2025-06-25 15:56:06 +0800

    react query provider

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import { reactQueryOptions } from '@/lib/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React, { useState } from 'react'
 
 interface IAppProvider {
   children: React.ReactNode;
 }
 
 const AppProvider: React.FC<IAppProvider> = ({ children }) => {
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: reactQueryOptions
+  }))
+  
   return (
-    <div>{children}</div>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
 }
 
