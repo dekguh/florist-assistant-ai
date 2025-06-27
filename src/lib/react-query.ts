c06ff800517ac169f78f70cf97b7e207cdd2a0cd
just kadek <kdkteguhmahesa@gmail.com>
2025-06-27 17:18:01 +0800

    react query options

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,1 +1,9 @@
-export const reactQueryOptions = {}
+import type { DefaultOptions } from '@tanstack/react-query'
+
+export const reactQueryOptions : DefaultOptions = {
+  queries: {
+    staleTime: 1000 * 60, // 1 hour
+    retry: (failurCount) => failurCount === 3,
+    refetchOnWindowFocus: true
+  }
+}
