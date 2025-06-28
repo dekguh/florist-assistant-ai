c2edd7bc95d49b8a7d597410f59fe4744831c63a
just kadek <kdkteguhmahesa@gmail.com>
2025-06-28 20:29:13 +0800

    fix mirage can call localhost n8n webhook

--- src/lib/mirage.ts
+++ src/lib/mirage.ts
@@ -1,9 +1,13 @@
+ 
+ 
 import { createServer } from 'miragejs'
 
 export const mirageServer = ({ environment = 'test' }) => createServer({
   environment,
   routes() {
+    this.urlPrefix = 'http://localhost:5173/'
     this.namespace = 'api'
+    this.passthrough('http://localhost:5678/**')
 
     this.post('/history-message/create', (schema, request) => {
       return schema.db.historyMessage.insert(JSON.parse(request.requestBody))
